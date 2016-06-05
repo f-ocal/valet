ded27c609017b1acc33f5c1f825f422e10a0c52a
Jim Fung <jimtheengineer@gmail.com>
2016-06-04 21:04:41 -0700

    Refactor

--- valet-api/server.js
+++ valet-api/server.js
@@ -1,42 +1,43 @@
-var http = require('https');
+var express  = require('express');
+var app = express();
+var ws = require('nodejs-websocket')
 var googleMaps = require('googlemaps');
 
 var getParkingCoordinates = function() {
   fakeResponse = {
     "event-uid": "360597e9-3a3d-4f1c-a686-4d575ba11f55",
-      "timestamp": 1461019279795,
-      "event-type": "PKOUT",
-      "device-uid": "HYP1040-75",
-      "location-uid": "158-parkingspot",
-      "properties": {
-        "object-uid": "cf0dfb91-b11e-4d4d-9213-eb057187122f",
-        "coordinates":
-          "32.711637:-117.15732968838816,32.711637100678395:-117.15736341303291,32.711734:-117.15736427535526,32.711733:-117.15733055071051"
-      },
-      "measures": [
-      {
-        "tag": "PKOVLP",
-        "value": 100,
-        "unit": "PCT"
-      }
+    "timestamp": 1461019279795,
+    "event-type": "PKOUT",
+    "device-uid": "HYP1040-75",
+    "location-uid": "158-parkingspot",
+    "properties": {
+      "object-uid": "cf0dfb91-b11e-4d4d-9213-eb057187122f",
+      "coordinates":
+        "32.711637:-117.15732968838816,32.711637100678395:-117.15736341303291,32.711734:-117.15736427535526,32.711733:-117.15733055071051"
+    },
+    "measures": [
+    {
+      "tag": "PKOVLP",
+      "value": 100,
+      "unit": "PCT"
+    }
     ]
   };
-  return fakeResponse;
+  return fakeResponse.properties.coordinates.split(',').map(function(o){ return o.split(':') });
 };
 
-var parkOutCoordinates = function(predixResponse) {
-  return predixResponse.properties.coordinates.split(',').map(function(o){ return o.split(':') });
-}
-var publicConfig = {
-  key: process.env.GOOGLE_MAPS_API_KEY,
-  stagger_time: 1000,
-  encode_polylines: false,
-  secure: true
-};
-
-var gmAPI = new googleMaps(publicConfig);
 var addressLookupFromCoordinates = function(lat, lon) {
+  var publicConfig = {
+    key: process.env.GOOGLE_MAPS_API_KEY,
+    stagger_time: 1000,
+    encode_polylines: false,
+    secure: true
+  };
+
+  var gmAPI = new googleMaps(publicConfig);
+
   var latlngStr = lat + ',' + lon;
+
   var lookup = {
     'latlng': latlngStr,
     'language': 'en',
@@ -46,13 +47,6 @@ var addressLookupFromCoordinates = function(lat, lon) {
     console.log(result.results[0].formatted_address);
   });
 }
-var parkingCoordinates = getParkingCoordinates();
-var [lat, lon] = parkOutCoordinates(parkingCoordinates)[0];
-console.log(addressLookupFromCoordinates(lat, lon));
-var express  = require('express');
-var app = express();
-
-var ws = require('nodejs-websocket')
 
 var predixToken = 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiIyNDMzMzBlZi04OWIzLTRkMjItOWEzYS1mNmYxZmIxOTg3MjYiLCJzdWIiOiJwYXJraW5naW5mb3JtYXRpb24iLCJzY29wZSI6WyJ1YWEucmVzb3VyY2UiLCJvcGVuaWQiLCJ1YWEubm9uZSIsImllLXBhcmtpbmcuem9uZXMuZTg0Y2FjYjMtNGM1Yi00YzMyLWI0NzAtY2Y2MzY1Y2I2MTI5LnVzZXIiXSwiY2xpZW50X2lkIjoicGFya2luZ2luZm9ybWF0aW9uIiwiY2lkIjoicGFya2luZ2luZm9ybWF0aW9uIiwiYXpwIjoicGFya2luZ2luZm9ybWF0aW9uIiwiZ3JhbnRfdHlwZSI6ImNsaWVudF9jcmVkZW50aWFscyIsInJldl9zaWciOiIyN2I3ZDZlMSIsImlhdCI6MTQ2MzA4MjQ2NCwiZXhwIjoxNDYzMTI1NjY0LCJpc3MiOiJodHRwczovL2UwYzI5NTY5LWI4OGQtNDY5NC04OTRkLTk4YmJjODlhYjM1MS5wcmVkaXgtdWFhLnJ1bi5hd3MtdXN3MDItcHIuaWNlLnByZWRpeC5pby9vYXV0aC90b2tlbiIsInppZCI6ImUwYzI5NTY5LWI4OGQtNDY5NC04OTRkLTk4YmJjODlhYjM1MSIsImF1ZCI6WyJwYXJraW5naW5mb3JtYXRpb24iLCJ1YWEiLCJvcGVuaWQiLCJpZS1wYXJraW5nLnpvbmVzLmU4NGNhY2IzLTRjNWItNGMzMi1iNDcwLWNmNjM2NWNiNjEyOSJdfQ.gMRNxKC_KR_STv5IxrvwNvgIPrXgb5-ylGhowJBfEhHraT4oHXMbfmBSPqqATTcZcgbhYe6OweRTAOJtUD0uotzBwN_X8O9loJkg21Xx8oOrMzZXzwYcIbAv0R2-15PRrsxybECQGfiu_5yCe3ag_AOHloAj_dbKCDCzoPgNYbNnx7zeDuY0U_3KTpqjELnWOQFhTkG5NH_6cAwKvzJQt3GdGhSa9TUsmqfrlhvgyb1_6Uk88OqsoDmtRXuEUg-iF6l0oWmfDL_6nR6Xy9iBI8PUnzC3rZyYLl_q3JPssZs9YJx0dY1uYmd7ZT9UHlYnQf_K-MNmMQxPEyx0tfkmKg'
 
